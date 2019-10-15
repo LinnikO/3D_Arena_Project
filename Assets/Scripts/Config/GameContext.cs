5ddb8e8b5506821ad9331339a00ec4cd84c8cc72
Oles Linnik <linnikoles@gmail.com>
2019-10-15 13:23:01 +0300

    GameContext added

---
+++ Assets/Scripts/Config/GameContext.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using strange.extensions.context.impl;
+using strange.extensions.command.api;
+using strange.extensions.command.impl;
+using strange.extensions.context.api;
+
+public class GameContext : MVCSContext
+{
+    public GameContext(MonoBehaviour contextView, ContextStartupFlags flag) : base(contextView, flag)
+    {}
+
+
+    protected override void mapBindings()
+    {
+        
+    }
+
+    protected override void addCoreComponents()
+    {
+        base.addCoreComponents();
+        injectionBinder.Unbind<ICommandBinder>();
+        injectionBinder.Bind<ICommandBinder>().To<SignalCommandBinder>().ToSingleton();
+    }
+
+    public override void Launch()
+    {
+        base.Launch();
+        
+    }
+}
---
+++ Assets/Scripts/View/GameContextView.cs
@@ -0,0 +1,13 @@
+﻿using strange.extensions.context.impl;
+using UnityEngine;
+using strange.extensions.context.api;
+
+public class GameContextView : ContextView
+{
+    private void Awake()
+    {
+        context = new GameContext(this, ContextStartupFlags.MANUAL_MAPPING);
+        context.Start();
+    }
+}
+
